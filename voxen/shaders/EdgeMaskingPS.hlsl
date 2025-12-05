e764c51bb39156e52c046252749f0d7f42ee62b7
malbong <gon3578@gmail.com>
2025-12-05 20:38:53 +0900

    FEAT: edge masking에 대한 추가 검사 진행

--- voxen/shaders/EdgeMaskingPS.hlsl
+++ voxen/shaders/EdgeMaskingPS.hlsl
@@ -12,18 +12,39 @@ struct psInput
 float4 main(psInput input) : SV_Target
 {
     float sumEdge = 0;
-    float sumW = 0;
+    float invaildPos = 0;
     
     [unroll]
     for (uint i = 0; i < SAMPLE_COUNT; ++i)
     {
-        sumEdge += normalEdgeTex.Load(input.posProj.xy, i).w;
-        sumW += positionTex.Load(input.posProj.xy, i).w;
+        invaildPos += positionTex.Load(input.posProj.xy, i).w;
+        
+        float4 sample = normalEdgeTex.Load(input.posProj.xy, i);
+        
+        uint edge = sample.w; // edge 1, no edge 0, invaild -1
+        sumEdge += (edge > 0) ? 1 : 0;
     }
-    
     if (!sumEdge)
         discard;
-    if (sumW == -1.0 * SAMPLE_COUNT)
+    if (invaildPos == -1.0 * SAMPLE_COUNT)
+        discard;
+    
+    bool flat = true;
+    bool close = true;
+    float3 baseNormal = normalEdgeTex.Load(input.posProj.xy, 0).xyz;
+    float3 basePosition = positionTex.Load(input.posProj.xy, 0).xyz;
+    [unroll]
+    for (uint j = 1; j < SAMPLE_COUNT; ++j)
+    {
+        float angle = dot(baseNormal, normalEdgeTex.Load(input.posProj.xy, j).xyz);
+        if (angle < 0.98)
+            flat = false;
+        
+        float dist = length(basePosition - positionTex.Load(input.posProj.xy, j).xyz);
+        if (dist > 1.0)
+            close = false;
+    }
+    if (flat && close)
         discard;
     
     return float4(1, 0, 0, 0);
